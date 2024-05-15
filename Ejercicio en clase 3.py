94fed23c090e85a82823e5c63e48dda5351580e3
WilmerRamos21 <168014691+WilmerRamos21@users.noreply.github.com>
2024-05-15 12:04:11 -0500

    Add files via upload

---
+++ Ejercicio en clase 3.py
@@ -0,0 +1,25 @@
+
+
+print("Instagram")
+
+user= input("Phone number, username or email")
+password= input("Password")
+
+if user== "Adrian" and password =="123":
+    print(f"Welcome {user}")
+    print("Loading your posts")
+else:
+    print("Sorry, your password was incorrect. Please double-check ypur password.")
+
+
+print("Sign up to see photos and videos from your friends.")
+input("Log in with Facebook")
+facebook= input("Yes or No")
+if facebook=="si":
+    print("Welcome")
+else:
+    mobile= input("Mobile number or email")
+    name= input("Full name")
+    username= input("Username")
+    password= input("password")
+    
